1d69247c8f44d04f2fa63db969a48d83c5ebe956
massissiliachl <massissilia.chalal@se.univ-bejaia.dz>
2026-02-25 21:31:25 +0100

    fix production coig

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,15 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize('visitbejaia', 'postgres', 'massissilia06', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false, // pour ne pas afficher toutes les requêtes SQL
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false
 });
 
 module.exports = sequelize;
