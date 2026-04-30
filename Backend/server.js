6c88d053e57bae8e4f72f93f9d83e8f8b9b83e46
massissiliachl <massissilia.chalal@se.univ-bejaia.dz>
2026-04-30 14:34:24 +0200

    ajout de ma é

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,9 @@ app.use("/availability", availabilityRoutes);
 // MAIL CONFIG
 // ============================
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
